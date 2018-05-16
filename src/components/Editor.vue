e238925dad83b192abe12ca0fc56a8e9dec44745
kokushin <mail@kokush.in>
2018-05-16 19:36:33 +0900

    update

---
+++ src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="editor">
+    <h1>Editor</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'editor',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
